911ec225fe0e94630b46d36a947de5f7dbf732d8
XBT1 <xbtop1@gmail.com>
2018-01-01 23:59:55 +0800

    feat: 修复 random hover 样式问题 & 添加文档

--- README.md
+++ README.md
@@ -6,47 +6,88 @@
 ![Dependencies][badge-dependencies]
 ![License][badge-license]
 
-[![NPM][image-npm]][url-npm]
+适用于 React 的优雅而炫酷的 Dropdown 组件。
 
-Simple effects of Dropdown for React.js.。
+[![NPM][image-npm]][url-npm]
 
 **中文 | [English](./README_en.md)**
 
 ## 安装
 
 ```bash
-$ yarn add effect-dropdown-react # npm i -S effect-dropdown-react
+$ npm i -S effect-dropdown-react # yarn add effect-dropdown-react
 ```
 
 ## 概览
 
-[访问在线示例]()
+![概览](./docs/images/gallery.gif)
+
+[访问在线示例](https://xbt1.github.io/effect-dropdown-react/)
 
 ## 使用
 
 一个简单的例子
 
+`main.js`
+
 ```javascript
+  import 'effect-dropdown-react/dist/index.css'
 ```
 
-```html
-<template>
-</template>
+`App.jsx`
+
+```javascript
+  import { EffectDropdown, EffectDropdownItem } from 'effect-dropdown-react'
+
+  export default () => {
+    return (
+      <EffectDropdown
+        effect="camber"
+        activeColor="#4d8c9d"
+        label="Select your platform"
+      >
+        <EffectDropdownItem>Twitter</EffectDropdownItem>
+        <EffectDropdownItem>Facebook</EffectDropdownItem>
+        <EffectDropdownItem>Google+</EffectDropdownItem>
+        <EffectDropdownItem>GitHub</EffectDropdownItem>
+      </EffectDropdown>
+    )
+  }
 ```
+
+详细使用方法见 [文档](./docs/usage.md) & [例子](./examples)
+
+## Vue
+
+[查看 Vue 版本](https://github.com/XBT1/effect-dropdown-vue)
 
 ## 开发
 
 ```bash
-$ yarn install
-$ yarn dev
+$ npm install
+$ npm run dev
+```
+
+## 调试
+
+```bash
+$ npm run build:package
+$ npm link ./
+```
+
+`App.jsx`
+
+```javascript
+  import { EffectDropdown, EffectDropdownItem } from 'effect-dropdown-react'
+  import 'effect-dropdown-react/dist/index.css'
 ```
 
 ## 构建
 
 ```bash
-$ yarn build:package # 构建 npm 包
-$ yarn build:example # 构建示例站点
-$ yarn build # build:package & build:example
+$ npm run build:package # 构建 npm 包
+$ npm run build:example # 构建示例站点
+$ npm run build # build:package & build:example
 ```
 
 ## 更新日志
@@ -57,11 +98,11 @@ $ yarn build # build:package & build:example
 [badge-npm-version]: https://img.shields.io/npm/v/effect-dropdown-react.svg
 [badge-node-version]: https://img.shields.io/node/v/effect-dropdown-react.svg
 [badge-npm-download]: https://img.shields.io/npm/dt/effect-dropdown-react.svg
-[badge-license]: https://img.shields.io/github/license/XBT1 <xbtop1@gmail.com>/effect-dropdown-react.svg
-[badge-dependencies]: https://img.shields.io/david/dev/XBT1 <xbtop1@gmail.com>/effect-dropdown-react.svg
+[badge-license]: https://img.shields.io/github/license/XBT1/effect-dropdown-react.svg
+[badge-dependencies]: https://img.shields.io/david/dev/XBT1/effect-dropdown-react.svg
 
 [url-npm]: https://npmjs.org/package/effect-dropdown-react
 [url-dependencies]: https://david-dm.org/vkbansal/effect-dropdown-react
-[url-releases]: https://github.com/XBT1 <xbtop1@gmail.com>/effect-dropdown-react/releases
+[url-releases]: https://github.com/XBT1/effect-dropdown-react/releases
 
 [image-npm]: https://nodei.co/npm/effect-dropdown-react.png
--- examples/App.jsx
+++ examples/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import { EffectDropdown, EffectDropdownItem } from '../src/index'
-
 import '../src/styles/index.styl'
+
+// import { EffectDropdown, EffectDropdownItem } from 'effect-dropdown-react'
+// import 'effect-dropdown-react/dist/index.css'
 
 const reactLogo = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K' // eslint-disable-line
 const githubLogoPath = 'M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z' // eslint-disable-line
@@ -53,7 +55,7 @@ const App = () => (
             className="source-link"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L64"
+            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L65"
 
           >
             源码
@@ -62,7 +64,7 @@ const App = () => (
 
         <div className="dropdown-main">
           <EffectDropdown label="Pick your direction" style={{ zIndex: 4000 }}>
-            <EffectDropdownItem>
+            <EffectDropdownItem onClick={(args) => { console.log(args) }}>
               <i className="ion-arrow-up-c" />
               <span>Up</span>
             </EffectDropdownItem>
@@ -90,7 +92,7 @@ const App = () => (
             className="source-link"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L100"
+            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L101"
           >
             源码
           </a>
@@ -114,7 +116,7 @@ const App = () => (
             className="source-link"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L124"
+            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L125"
           >
             源码
           </a>
@@ -151,7 +153,7 @@ const App = () => (
             className="source-link"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L161"
+            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L162"
           >
             源码
           </a>
@@ -193,7 +195,7 @@ const App = () => (
             className="source-link"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L203"
+            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L204"
           >
             源码
           </a>
@@ -238,7 +240,7 @@ const App = () => (
             className="source-link"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L248"
+            href="https://github.com/XBT1/effect-dropdown-react/blob/master/examples/App.jsx#L249"
           >
             源码
           </a>
